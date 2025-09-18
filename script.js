0eacaec3a3f372a6ecd0b266f121328df6243e98
Saad Hussain <saadcfi4@gmail.com>
2025-09-18 20:22:13 +0400

    abbreviatoon maker

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ Write a function that checks if a string is the same forwards and backwards.
 Input: "madam" → Output: true
 */
 
-function stringFrontAndBack(str) {
-  const strArr = str.split("");
-  const reversedArr = [];
-  for (let i = strArr.length - 1; i >= 0; i--) {
-    reversedArr.push(strArr[i]);
-  }
-  let revStr = reversedArr.join("");
-  return str === revStr;
-}
-console.log(stringFrontAndBack("hello"));
-console.log(stringFrontAndBack("madam"));
+// function stringFrontAndBack(str) {
+//   const strArr = str.split("");
+//   const reversedArr = [];
+//   for (let i = strArr.length - 1; i >= 0; i--) {
+//     reversedArr.push(strArr[i]);
+//   }
+//   let revStr = reversedArr.join("");
+//   return str === revStr;
+// }
+// console.log(stringFrontAndBack("hello"));
+// console.log(stringFrontAndBack("madam"));
 
 /*
 Count Vowels
@@ -24,28 +24,38 @@ Write a function that counts how many vowels are in a string.
 Input: "hello" → Output: 2
 */
 
-function countVowels(string) {
-  const vowels = [];
-  for (let i = 0; i < string.length; i++) {
-    if (
-      string[i] === "a" ||
-      string[i] === "e" ||
-      string[i] === "i" ||
-      string[i] === "o" ||
-      string[i] === "u"
-    ) {
-      vowels.push(string[i]);
-    }
-  }
-  return `Vowels in ${string}: ${vowels.length}`;
-}
-console.log(countVowels("hello"));
-console.log(countVowels("saad hussain"));
+// function countVowels(string) {
+//   const vowels = [];
+//   for (let i = 0; i < string.length; i++) {
+//     if (
+//       string[i] === "a" ||
+//       string[i] === "e" ||
+//       string[i] === "i" ||
+//       string[i] === "o" ||
+//       string[i] === "u"
+//     ) {
+//       vowels.push(string[i]);
+//     }
+//   }
+//   return `Vowels in ${string}: ${vowels.length}`;
+// }
+// console.log(countVowels("hello"));
+// console.log(countVowels("saad hussain"));
 
 /*
 Abbreviation Maker
 Input: "Java Script Object Notation" → Output: "JSON"
 */
+
+function abbvMaker(str) {
+  const words = [];
+  for (const word of str.split(" ")) {
+    words.push(word[0]);
+  }
+  return words.join("");
+}
+
+console.log(abbvMaker("Java Script Object Notation"));
 
 /*
 Hide Phone Number
