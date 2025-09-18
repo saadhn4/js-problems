963998a43f7e1ed1f38c84c4bedbd8bcd3a076e0
Saad Hussain <saadcfi4@gmail.com>
2025-09-18 20:32:16 +0400

    mask number function

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ Write a function that checks if a string is the same forwards and backwards.
 Input: "madam" → Output: true
 */
 
-// function stringFrontAndBack(str) {
-//   const strArr = str.split("");
-//   const reversedArr = [];
-//   for (let i = strArr.length - 1; i >= 0; i--) {
-//     reversedArr.push(strArr[i]);
-//   }
-//   let revStr = reversedArr.join("");
-//   return str === revStr;
-// }
-// console.log(stringFrontAndBack("hello"));
-// console.log(stringFrontAndBack("madam"));
+function stringFrontAndBack(str) {
+  const strArr = str.split("");
+  const reversedArr = [];
+  for (let i = strArr.length - 1; i >= 0; i--) {
+    reversedArr.push(strArr[i]);
+  }
+  let revStr = reversedArr.join("");
+  return str === revStr;
+}
+console.log(stringFrontAndBack("hello"));
+console.log(stringFrontAndBack("madam"));
 
 /*
 Count Vowels
@@ -24,23 +24,23 @@ Write a function that counts how many vowels are in a string.
 Input: "hello" → Output: 2
 */
 
-// function countVowels(string) {
-//   const vowels = [];
-//   for (let i = 0; i < string.length; i++) {
-//     if (
-//       string[i] === "a" ||
-//       string[i] === "e" ||
-//       string[i] === "i" ||
-//       string[i] === "o" ||
-//       string[i] === "u"
-//     ) {
-//       vowels.push(string[i]);
-//     }
-//   }
-//   return `Vowels in ${string}: ${vowels.length}`;
-// }
-// console.log(countVowels("hello"));
-// console.log(countVowels("saad hussain"));
+function countVowels(string) {
+  const vowels = [];
+  for (let i = 0; i < string.length; i++) {
+    if (
+      string[i] === "a" ||
+      string[i] === "e" ||
+      string[i] === "i" ||
+      string[i] === "o" ||
+      string[i] === "u"
+    ) {
+      vowels.push(string[i]);
+    }
+  }
+  return `Vowels in ${string}: ${vowels.length}`;
+}
+console.log(countVowels("hello"));
+console.log(countVowels("saad hussain"));
 
 /*
 Abbreviation Maker
@@ -61,3 +61,10 @@ console.log(abbvMaker("Java Script Object Notation"));
 Hide Phone Number
 Input: "9876543210" → Output: "*****43210"
 */
+
+function hideNumber(number) {
+  const n = String(number);
+  console.log(`${"*".repeat(n.length - 5)}${n.slice(-5)}`);
+}
+hideNumber(9876543210);
+hideNumber(1234543210);
